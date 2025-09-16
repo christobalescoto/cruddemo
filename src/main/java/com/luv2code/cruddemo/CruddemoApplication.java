beda39fc6a10ce465970a8f789baa684bd7c7b52
chris <cescot4@wgu.edu>
2025-09-16 03:36:47 -0400

    Updating Objects with JPA

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -28,8 +28,28 @@ public class CruddemoApplication {
 
             // queryForStudents(studentDAO);
 
-            queryForStudentsByLastName(studentDAO);
+            // queryForStudentsByLastName(studentDAO);
+
+            updateStudent(studentDAO);
         };
+    }
+
+    private void updateStudent(StudentDAO studentDAO) {
+
+        // retrieve student based on the id: primary key
+        int studentId = 1;
+        System.out.println("Getting student with id: " + studentId);
+        Student myStudent = studentDAO.findById(studentId);
+
+        // change the first name to "Scooby"
+        System.out.println("Updating student ...");
+        myStudent.setFirstName("John");
+
+        // update the student
+        studentDAO.update(myStudent);
+
+        // display the updated student
+        System.out.println("Updated student ..." + myStudent);
     }
 
     private void queryForStudentsByLastName(StudentDAO studentDAO) {
@@ -39,7 +59,6 @@ public class CruddemoApplication {
         // display list of students
         for (Student tempStudent : theStudents) {
             System.out.println(tempStudent);
-
         }
     }
 
